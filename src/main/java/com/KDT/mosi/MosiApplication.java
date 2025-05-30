e9fefa132b0f890f2bc46d98e871cacd1f1b8965
Jinhee Heo <thesolarsystem710@gmail.com>
2025-05-30 17:51:41 +0900

    초기상태

---
+++ src/main/java/com/KDT/mosi/MosiApplication.java
@@ -0,0 +1,13 @@
+package com.KDT.mosi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosiApplication.class, args);
+	}
+
+}
