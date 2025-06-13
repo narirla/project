6f5b2e89b3f9702effe6fab1695dadaf33658b6c
Jinhee Heo <thesolarsystem710@gmail.com>
2025-06-13 17:40:44 +0900

    sql

---
+++ src/test/java/com/KDT/mosi/MosiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KDT.mosi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
