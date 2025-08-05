1a83edb1d82486220f6e067d6b8c88d0ec54b640
Jinhee Heo <thesolarsystem710@gmail.com>
2025-08-05 16:47:22 +0900

    elastic_intergration

--- src/main/java/com/KDT/mosi/web/controller/AccessibilityController.java
+++ src/main/java/com/KDT/mosi/web/controller/AccessibilityController.java
@@ -1,8 +1,6 @@
 package com.KDT.mosi.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AccessibilityController {
 
   // 편의시설 대시보드 페이지
-  @GetMapping("/accessibility")
-  public String accessibilityDashboard(Model model) {
-
-    return "info/accessibility";
-  }
+//  @GetMapping("/accessibility")
+//  public String accessibilityDashboard(Model model) {
+//
+//    return "info/accessibility";
+//  }
 }
--- src/main/java/com/KDT/mosi/web/controller/RestaurantPageController.java
+++ src/main/java/com/KDT/mosi/web/controller/RestaurantPageController.java
@@ -3,6 +3,7 @@ package com.KDT.mosi.web.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,8 +14,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class RestaurantPageController {
 
+
+  //맛집 페이지
   @GetMapping("/restaurant")
   public String showMapPage() {
     return "info/info-restaurant";  // templates/info/info-restaurant.html
   }
+
+  // 편의시설 대시보드 페이지
+  @GetMapping("/accessibility")
+  public String accessibilityDashboard(Model model) {
+
+    return "info/accessibility";
+  }
+
+
 }
+
+
