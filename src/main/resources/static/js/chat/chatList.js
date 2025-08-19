534484e8322efeffd418f679652c0fd866aaa337
ji-nious <thesolarsystem710@gmail.com>
2025-08-19 21:32:35 +0900

    세번째

--- src/main/resources/static/js/chat/chatList.js
+++ src/main/resources/static/js/chat/chatList.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const sellerId = document.body.dataset.sellerId; // body 태그에 sellerId를 주입해둔다
+//    const sellerId = document.body.dataset.sellerId; // body 태그에 sellerId를 주입해둔다
     const listDiv = document.getElementById("room-list"); // 채팅방 목록 출력 영역
 
     // ✅ 1. WebSocket 연결
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("✅ WebSocket 연결됨");
 
         // ✅ 2. 판매자 전용 채널 구독
-        stompClient.subscribe(`/chatList/${sellerId}`, (message) => {
+        stompClient.subscribe(`/chat/rooms`, (message) => {
             const roomDto = JSON.parse(message.body);
             console.log("📩 새 채팅방 알림:", roomDto);
 
@@ -24,38 +24,46 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // ✅ 3. Ajax로 채팅방 목록 불러오기
     function refreshRoomList() {
-        fetch(`/chat/rooms/api?sellerId=${sellerId}`)
+        fetch(`/chat/rooms/api`)
             .then(res => res.json())
             .then(data => {
                 console.log("📋 현재 채팅방 목록:", data);
                 renderRoomList(data);
             })
-            .catch(err => console.error("❌ 채팅방 목록 조회 실패", err));
+            .catch(err => {
+            console.error("❌ 채팅방 목록 조회 실패", err)
+            console.error(err.stack);
+            });
     }
 
     // ✅ 4. 화면에 목록 그려주기
     function renderRoomList(data) {
         listDiv.innerHTML = ""; // 기존 목록 지우기
         if (data.length === 0) {
-            listDiv.innerHTML = "<p>받은 문의가 없습니다.</p>";
+            listDiv.innerHTML = "<li>받은 문의가 없습니다.</li>";
             return;
         }
 
         data.forEach(room => {
-            const div = document.createElement("div");
-            div.classList.add("room-item");
-            div.innerHTML = `
-                <b>상품ID: ${room.productId}</b><br/>
-                구매자: ${room.buyerId}<br/>
-                채팅방ID: ${room.roomId}
+            const li = document.createElement("li");
+//            div.classList.add("room-item");
+            li.innerHTML = `
+                <b>상품ID:</b> ${room.productId}<br/>
+                <b>구매자:</b> ${room.buyerId}<br/>
+                <b>채팅방ID:</b> ${room.roomId}
             `;
             // 클릭하면 채팅창 팝업 띄우기
-            div.addEventListener("click", () => {
+            li.addEventListener("click", () => {
                 window.open(`/chat/popup?roomId=${room.roomId}`,
                     "chatPopup",
                     "width=400,height=600");
             });
-            listDiv.appendChild(div);
+            listDiv.appendChild(li);
         });
     }
+
+    // 페이지 로스 시 Ajax 호출
+//    refreshRoomList();
+
+
 });
