f184d562fc2bb60f1114f592a198f7906bdcc2e9
jihye9427 <vmflsxj456@naver.com>
2025-08-20 21:07:22 +0900

    250820-03

--- src/main/java/com/KDT/mosi/web/controller/CartController.java
+++ src/main/java/com/KDT/mosi/web/controller/CartController.java
@@ -10,6 +10,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
